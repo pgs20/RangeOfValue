9edc73d5d8f655ad6049fdc1941bb34ebe736733
Gosha <gosha_petrov20@rambler.ru>
2024-03-06 21:08:52 +0300

    Реализовал поиск размера наибольшего промежутка, состоящего из символов 'а' с помощью потоков. Время реализации с помощью потоков примерно 54600 мс, без потоков - 86600 мс.

---
+++ src/main/java/MyThread.java
@@ -0,0 +1,30 @@
+public class MyThread extends Thread {
+    private String text;
+
+    public MyThread(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public void run() {
+        int maxSize = 0;
+        for (int i = 0; i < text.length(); i++) {
+            for (int j = 0; j < text.length(); j++) {
+                if (i >= j) {
+                    continue;
+                }
+                boolean bFound = false;
+                for (int k = i; k < j; k++) {
+                    if (text.charAt(k) == 'b') {
+                        bFound = true;
+                        break;
+                    }
+                }
+                if (!bFound && maxSize < j - i) {
+                    maxSize = j - i;
+                }
+            }
+        }
+        System.out.println(text.substring(0, 100) + " -> " + maxSize);
+    }
+}
